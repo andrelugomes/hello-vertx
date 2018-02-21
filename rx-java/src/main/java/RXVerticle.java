a53a0c202a44ebcf580c5ade936fa33ef684bf5d
Andre Gomes <andre.gomes@s2it.com.br>
2018-02-20 22:29:51 -0300

    add request retry on handler

--- rx-java/src/main/java/RXVerticle.java
+++ rx-java/src/main/java/RXVerticle.java
@@ -1,6 +1,6 @@
 import io.vertx.core.json.JsonObject;
 import io.vertx.rxjava.core.AbstractVerticle;
-import io.vertx.rxjava.core.http.HttpServer;
+import io.vertx.rxjava.core.RxHelper;
 import io.vertx.rxjava.ext.web.Router;
 import io.vertx.rxjava.ext.web.RoutingContext;
 import io.vertx.rxjava.ext.web.client.HttpRequest;
@@ -8,6 +8,8 @@ import io.vertx.rxjava.ext.web.client.HttpResponse;
 import io.vertx.rxjava.ext.web.client.WebClient;
 import io.vertx.rxjava.ext.web.codec.BodyCodec;
 import rx.Single;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by andre on 18/02/18.
@@ -20,15 +22,17 @@ public class RXVerticle extends AbstractVerticle {
     public void start() {
         client = WebClient.create(vertx);
         Router router = Router.router(vertx);
-        router.get("/multiples-requests").handler(this::invokeMicroservices);
+        router.get("/single-zip-requests").handler(this::singleZipRequests);
+        router.get("/retry-requests").handler(this::retryRequests);
         vertx.createHttpServer().requestHandler(router::accept).listen(8080);
     }
 
-    private void invokeMicroservices(RoutingContext rc) {
+    private void singleZipRequests(RoutingContext rc) {
         HttpRequest<JsonObject> request1 = client.get(8081, "localhost", "/products/prod3568").as(BodyCodec.jsonObject());
         HttpRequest<JsonObject> request2 = client.get(8081, "localhost", "/products/prod7340").as(BodyCodec.jsonObject());
         Single<JsonObject> s1 = request1.rxSend().map(HttpResponse::body);
         Single<JsonObject> s2 = request2.rxSend().map(HttpResponse::body);
+
         Single.zip(s1, s2, (p1, p2) -> {
             // We have the results of both requests
             return new JsonObject()
@@ -38,5 +42,16 @@ public class RXVerticle extends AbstractVerticle {
             error.printStackTrace();
             rc.response().setStatusCode(500).end(error.getMessage());
         });
+    }
+
+    private void retryRequests(RoutingContext rc) {
+        client.get(8081, "localhost", "/products/prod3568").as(BodyCodec.jsonObject())
+            .rxSend()
+            .subscribeOn(RxHelper.scheduler(vertx))
+            .timeout(5, TimeUnit.SECONDS)
+            .retry(1)
+            .map(HttpResponse::body)
+            .onErrorReturn(t -> new JsonObject().put("prod3568", "D'oh! Timeout"))
+            .subscribe(json -> json.encode());
     }
 }
